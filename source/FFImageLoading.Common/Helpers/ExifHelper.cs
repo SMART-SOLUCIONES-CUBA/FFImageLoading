8355bbb5d049fc816240a1ee2fc5c12894c2b41f
Miguel <miguel.torres.ss24@gmail.com>
2021-11-24 08:54:21 -0500

    Actualizando para entorno

--- source/FFImageLoading.Common/Helpers/ExifHelper.cs
+++ source/FFImageLoading.Common/Helpers/ExifHelper.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using FFImageLoading.Helpers.Exif;
 
 namespace FFImageLoading.Helpers
 {
@@ -12,18 +9,18 @@ namespace FFImageLoading.Helpers
         //const int MOTOROLA_TIFF_MAGIC_NUMBER = 0x4D4D;
         //const int INTEL_TIFF_MAGIC_NUMBER = 0x4949;
 
-        static readonly ExifReader _exifReader = new ExifReader();
+        static readonly Exif.ExifReader _exifReader = new Exif.ExifReader();
 
-        public static IList<Directory> Read(Stream stream)
+        public static IList<Exif.Directory> Read(Stream stream)
         {
             if (stream.Position != 0)
                 stream.Position = 0;
 
 
             var segmentTypes = _exifReader.SegmentTypes;
-            var segments = JpegSegmentReader.ReadSegments(new SequentialStreamReader(stream), segmentTypes);
+            var segments = Exif.JpegSegmentReader.ReadSegments(new Exif.SequentialStreamReader(stream), segmentTypes);
 
-            var directories = new List<Directory>();
+            var directories = new List<Exif.Directory>();
 
             var readerSegmentTypes = _exifReader.SegmentTypes;
             var readerSegments = segments.Where(s => readerSegmentTypes.Contains(s.Type));
